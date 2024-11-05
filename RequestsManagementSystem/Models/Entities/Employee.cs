7f6ddc307e19ff06c5890fe717705a047aadec79
ammar180 <ammaromran360@gmail.com>
2024-11-05 21:01:15 +0200

    Add project files.

---
+++ RequestsManagementSystem/Models/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RequestsManagementSystem.Models.Entities
+{
+	public class Employee
+	{
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int VacationsBalance { get; set; }
+        public DateOnly DateOfEmployment { get; set;}
+        public string DepartmentName { get; set; }
+        [ForeignKey("EmployeeId")]
+        public int ManagerId { get; set; }
+    }
+}
---
+++ RequestsManagementSystem/Program.cs
@@ -0,0 +1,91 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using RequestsManagementSystem.Models;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
+var key = Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]);
+var ValidAudience = builder.Configuration["Jwt:Audience"];
+var ValidIssuer = builder.Configuration["Jwt:Issuer"];
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(op => op.UseSqlServer(connectionStr));
+
+// Add services to the container.
+
+// Add Authentication with JWT Bearer
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+	options.RequireHttpsMetadata = false;
+	options.SaveToken = true;
+	options.TokenValidationParameters = new TokenValidationParameters()
+	{
+		ValidateIssuerSigningKey = true,
+		IssuerSigningKey = new SymmetricSecurityKey(key),
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidAudience = ValidAudience,
+		ValidIssuer = ValidIssuer,
+	};
+});
+builder.Services.AddSwaggerGen(option =>
+{
+	option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+	option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Description = "Please enter a valid token",
+		Name = "Authorization",
+		Type = SecuritySchemeType.Http,
+		BearerFormat = "JWT",
+		Scheme = "Bearer"
+	});
+	option.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type=ReferenceType.SecurityScheme,
+					Id="Bearer"
+				}
+			},
+			new string[]{}
+		}
+	});
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
