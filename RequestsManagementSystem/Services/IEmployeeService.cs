99d7318aba80067cff97250ae06d22952a8c34d6
ammar180 <ammaromran.work@gmail.com>
2024-11-18 09:51:48 +0200

    Solve Cyclic porject references and back to .NET V8

---
+++ RequestsManagementSystem/Services/IEmployeeService.cs
@@ -0,0 +1,9 @@
+﻿using RequestsManagementSystem.Dtos.EmployeeDtos;
+
+namespace RequestsManagementSystem
+{
+    public interface IEmployeeService
+    {
+        Task<string> LoginAsync(LoginEmployeeDto loginEmployeeDto);
+    }
+}
