b61fe86f9b4c73aef5a854925a59a3c9011e657b
Hosny-Mohammed <hosnymohammed2007@gmail.com>
2025-03-13 21:50:29 +0200

    add transactions dtos

---
+++ RequestsManagementSystem.DTOs/api/TransactionsDtos/ReportTransactionDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestsManagementSystem.DTOs.api.TransactionsDtos
+{
+    class ReportTransactionDTO
+    {
+        public string Title { get; set; }
+        public string TotalLeaves { get; set; }
+        public string UsedLeaves { get; set; }
+        public string AdditionalLeaves { get; set; }
+        public List<TransactionForReportDTO> Transactions { get; set; }
+    }
+}
---
+++ RequestsManagementSystem.DTOs/api/TransactionsDtos/TransactionForReportDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestsManagementSystem.DTOs.api.TransactionsDtos
+{
+    class TransactionForReportDTO
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Duration { get; set; }
+    }
+}
